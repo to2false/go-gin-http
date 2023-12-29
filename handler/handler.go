83560ec110a722413f19091eb25a498b379c0b40
to2false <to2false@gmail.com>
2023-12-29 13:34:08 +0800

    feat: add gin handler wrap

---
+++ handler/handler.go
@@ -0,0 +1,108 @@
+package handler
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/to2false/go-gin-http/message"
+	"github.com/to2false/go-gin-http/validate"
+	"google.golang.org/protobuf/proto"
+)
+
+type (
+	Request interface {
+		proto.Message
+		Validate() error
+	}
+
+	Response proto.Message
+)
+
+func GinHandlerWrap[REQ Request, RESP Response](
+	transformerName string,
+	handlerFn func(context.Context, REQ) (RESP, error),
+) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		transformer := message.GetTransformer(transformerName)
+
+		if err := transformer.PreProcessRequest(c.Request); err != nil {
+			statusCode, data, e := transformer.Err(c.Request.Context(), err)
+			if e != nil {
+				c.JSON(statusCode, e.Error())
+				c.Abort()
+
+				return
+			}
+
+			c.Data(statusCode, transformer.ContentType(), data)
+			c.Abort()
+
+			return
+		}
+
+		var in REQ
+		if err := c.ShouldBind(in); err != nil {
+			statusCode, data, e := transformer.Err(c.Request.Context(), err)
+			if e != nil {
+				c.JSON(statusCode, e.Error())
+				c.Abort()
+
+				return
+			}
+
+			c.Data(statusCode, transformer.ContentType(), data)
+			c.Abort()
+
+			return
+		}
+
+		if err := in.Validate(); err != nil {
+			statusCode, data, e := transformer.Err(c.Request.Context(), validate.NewValidateError(err.Error()))
+			if e != nil {
+				c.JSON(statusCode, e.Error())
+				c.Abort()
+
+				return
+			}
+
+			c.Data(statusCode, transformer.ContentType(), data)
+			c.Abort()
+
+			return
+		}
+
+		out, err := handlerFn(c.Request.Context(), in)
+		if err != nil {
+			statusCode, data, e := transformer.Err(c.Request.Context(), err)
+			if e != nil {
+				c.JSON(statusCode, e.Error())
+				c.Abort()
+
+				return
+			}
+
+			c.Data(statusCode, transformer.ContentType(), data)
+			c.Abort()
+
+			return
+		}
+
+		httpStatusCode, data, err := transformer.Transform(c.Request.Context(), out)
+		if err != nil {
+			statusCode, data, e := transformer.Err(c.Request.Context(), err)
+			if e != nil {
+				c.JSON(statusCode, e.Error())
+				c.Abort()
+
+				return
+			}
+
+			c.Data(statusCode, transformer.ContentType(), data)
+			c.Abort()
+
+			return
+		}
+
+		c.Data(httpStatusCode, transformer.ContentType(), data)
+		c.Abort()
+	}
+}
